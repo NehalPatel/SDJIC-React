9ef7b4786259b14d261c2bc4336d73665d95a113
Nehal Patel <iamnehalpatel@gmail.com>
2023-07-31 17:57:42 +0530

    Use effects added

--- src/Pages/Learn/Demo/UseEffectDemo1.js
+++ src/Pages/Learn/Demo/UseEffectDemo1.js
@@ -1,18 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function UseEffectDemo1() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
 
-    setTimeout(() => {
-        setCount((count) => count + 1);
-    }, 1000);
+    useEffect(()=>{
+        document.title = `Count: ${count}`;
+        console.log('state variable changes');
+    },[count]);
+    //try using dependencies
+    //[], [count], [count, name]
 
     return (
         <>
             <h3>Demo 1</h3>
-
-            <p>I've rendered {count} times!</p>
-
+            <b>Counter : {count}</b> <br />
+            <button
+                className="btn btn-primary mx-2"
+                onClick={() => setCount(count+1)}
+            >
+                +1
+            </button>
+            <button
+                className="btn btn-primary mx-2"
+                onClick={() => setCount(count+10)}
+            >
+                +10
+            </button>
+            <button
+                className="btn btn-primary mx-2"
+                onClick={() => setCount(count+100)}
+            >
+                +100
+            </button>
+            <br />
+            <p className="text-dark mt-4">Hello {name},</p>
+            <input
+                className="form-input"
+                type="text"
+                value={name}
+                placeholder="Enter your name"
+                onChange={(e) => setName(e.target.value)}
+            />
         </>
     );
 }
